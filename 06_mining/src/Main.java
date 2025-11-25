a462d033bc8193d12d4978bfac88bcbcf52e4e90
Cristina <cristina.csevi@gmail.com>
2025-11-25 18:53:13 +0100

    Create 06_mining

---
+++ 06_mining/src/Main.java
@@ -0,0 +1,17 @@
+import java.security.NoSuchAlgorithmException;
+
+//public class Main {
+//    public static void main(String[] args) throws NoSuchAlgorithmException {
+//        IO.println("Calculando hashes del 0 al 200");
+//        HashCalculator.calculateHash("Cadena de ejemplo", 0, 200);
+//    }
+//}
+
+void main() throws NoSuchAlgorithmException {
+      IO.println("Calculando hashes del 0 al 100");
+      int solution = HashCalculator.calculateHash("Cadena de ejemplo", 0, 200);
+
+      if(HashCalculator.validate("Cadena de ejemplo", solution)) {
+        IO.println("Solución válida: " +  solution);
+      }
+}
