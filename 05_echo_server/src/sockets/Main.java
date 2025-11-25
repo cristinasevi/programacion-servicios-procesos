85e0590ba15b35d7e84161ec64eee770763c0da1
Cristina <cristina.csevi@gmail.com>
2025-11-25 12:29:05 +0100

    Create 05_echo_server

---
+++ 05_echo_server/src/sockets/Main.java
@@ -0,0 +1,9 @@
+package sockets;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new EchoServer().start(3000);
+    }
+}
